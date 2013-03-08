50b3e3a9db602d0e8739af23353ec85eae19c821
Exikle <dixondcunha@hotmail.com>
2013-03-08 02:43:45 -0600

    Reformatted the states class

--- Hangman/src/com/xid_studios/java/game/hangman/State.java
+++ Hangman/src/com/xid_studios/java/game/hangman/State.java
@@ -5,10 +5,10 @@ package com.xid_studios.java.game.hangman;
  */
 public enum State {
     START_MENU,
-    PLAYERONE_MENU,
-    PLAYERONE_CATEGORY,
-    PLAYERTWO_MENU,
-    PLAYERTWO_CATEGORY,
+    PLAYER_ONE_MENU,
+    PLAYER_ONE_CATEGORY,
+    PLAYER_TWO_MENU,
+    PLAYER_TWO_CATEGORY,
     WIN_SCREEN,
     LOSE_SCREEN,
     PLAY_SCREEN
